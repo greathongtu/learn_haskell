4df8a98e2b8dacb48f379bbb565fa782d6a93572
xtR0d666 <nicolasshuai@gmail.com>
2023-04-26 17:20:06 +0800

    recursion

--- baby.hs
+++ baby.hs
@@ -1,3 +1,6 @@
+-- ghci :l baby.hs
+-- :r to reload
+
 -- functions:
 doubleMe x = x + x  
 doubleUs x y = x*2 + y*2   
@@ -23,3 +26,117 @@ addThree x y z = x + y + z
 
 -- ghci> :t (>)  
 -- (>) :: (Ord a) => a -> a -> Bool  
+
+-- pattern matching
+lucky :: (Integral a) => a -> String  
+lucky 7 = "LUCKY NUMBER SEVEN!"  
+lucky x = "Sorry, you're out of luck, pal!"  
+
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x 
+
+-- guards
+-- using guards instead of patterns for a boolean condition.
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+
+-- where
+bmiTell' :: (RealFloat a) => a -> a -> String  
+bmiTell' weight height  
+    | bmi <= skinny = "You're underweight, you emo, you!"  
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= fat    = "You're fat! Lose some weight, fatty!"  
+    | otherwise     = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0  
+
+-- let <bindings> in <expression>
+-- let bindings are expressions themselves. where bindings are just syntactic constructs.
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea  
+
+-- case expressions
+-- case expression of pattern -> result
+-- can convert to normal pattern matching
+
+-- case expression of pattern -> result  
+--                    pattern -> result  
+--                    pattern -> result  
+--                    ...  
+
+
+
+-- recursion
+
+-- recursive function
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1) 
+
+length' :: (Num b) => [a] -> b  
+length' [] = 0  
+length' (_:xs) = 1 + length' xs  
+
+sum' :: (Num a) => [a] -> a  
+sum' [] = 0  
+sum' (x:xs) = x + sum' xs  
+
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs  
+
+maximum'' :: (Ord a) => [a] -> a  
+maximum'' [] = error "maximum of empty list"  
+maximum'' [x] = x  
+maximum'' (x:xs) = max x (maximum'' xs)  
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]  
+replicate' n x  
+    | n <= 0    = []  
+    | otherwise = x:replicate' (n-1) x  
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]  
+take' n _  
+    | n <= 0   = []  
+take' _ []     = []  
+take' n (x:xs) = x : take' (n-1) xs  
+
+reverse' :: [a] -> [a]  
+reverse' [] = []  
+reverse' (x:xs) = reverse' xs ++ [x]  
+
+repeat' :: a -> [a]  
+repeat' x = x:repeat' x  
+
+zip' :: [a] -> [b] -> [(a,b)]  
+zip' _ [] = []  
+zip' [] _ = []  
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys  
+
+elem' :: (Eq a) => a -> [a] -> Bool  
+elem' a [] = False  
+elem' a (x:xs)  
+    | a == x    = True  
+    | otherwise = a `elem'` xs  
+
+-- quicksort
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
